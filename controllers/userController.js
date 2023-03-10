0915803fb7901358249390bc838e53a5c63e7199
rhsb <ovillafana65@gmail.com>
2023-03-10 11:17:25 -0800

    2 commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,25 @@
 const { User } = require('../models');
+const md5 = require('md5');
 
 module.exports.renderRegistration = function(req, res){
     res.render('users/register');
 }
 
 module.exports.register = async function(req,res){
-    await User.create({
-        email: req.body.email,
-        password: req.body.password,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-    })
-    res.redirect('/');
-}
-
-module.exports.register = async function(req,res){
     const existingUser = await User.findOne({
         where:{
+            password: md5(req.body.password),
             email:req.body.email
         }
     });
     if (existingUser){
-        re.render('users/register', {
+        res.render('users/register', {
             error: 'User Already Exists'
         })
     } else{
         await User.create({
             email:req.body.email,
-            password:req.body.password,
+            password:md5(req.body.password),
             first_name:req.body.first_name,
             last_name:req.body.last_name,
         });
